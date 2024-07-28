74bb412ea03c0d2afafcb28b909d02ad000c301d
raminme <ramoures@gmail.com>
2024-07-29 02:32:21 +0330

    update notFound page

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,4 +1,13 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-    return "not found."
+    return <>
+        <div className="w-full flex items-center justify-center pt-20">
+            <div className="flex flex-col items-center gap-20">
+                <h2 className="bg-gray-500 rounded p-3 text-xl xl:text-4xl rotate-6 text-white">404, Page not found!</h2>
+                <Link to={`/`}>- Back to home -</Link >
+            </div>
+        </div>
+    </>
 }
 export default NotFound;
