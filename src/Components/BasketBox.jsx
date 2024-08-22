72d628db1ee955cb105838f4c631272721f320a0
raminme <ramoures@gmail.com>
2024-08-22 08:38:09 +0330

    update basketBox.jsx

--- src/Components/BasketBox.jsx
+++ src/Components/BasketBox.jsx
@@ -28,21 +28,20 @@ const BasketBox = () => {
                 setPrices([...itemPrices])
         }
     }, [])
-    const showbasket = () => {
-        document.getElementById('basketBox').classList.remove('hidden')
-        document.getElementById('basketBox').classList.add('flex')
-        document.getElementById('toggleBasket').classList.remove('rounded-lg')
-        document.getElementById('toggleBasket').classList.add('rounded-t-xl')
+    const showBasketBox = () => {
+        document.getElementById('basketBox').classList.remove('hidden');
+        document.getElementById('basketBox').classList.add('flex');
+        document.getElementById('toggleBasket').classList.remove('rounded-lg');
+        document.getElementById('toggleBasket').classList.add('rounded-t-xl');
     }
-    const hidebasket = (e) => {
-        document.getElementById('basketBox').classList.remove('flex')
-        document.getElementById('basketBox').classList.add('hidden')
-        document.getElementById('toggleBasket').classList.remove('rounded-t-xl')
-        document.getElementById('toggleBasket').classList.add('rounded-lg')
-
+    const hideBasketBox = (e) => {
+        document.getElementById('basketBox').classList.remove('flex');
+        document.getElementById('basketBox').classList.add('hidden');
+        document.getElementById('toggleBasket').classList.remove('rounded-t-xl');
+        document.getElementById('toggleBasket').classList.add('rounded-lg');
     }
     return (
-        <div id="toggleBasket" className="flex p-2 items-center gap-1 bg-white relative rounded-lg" onMouseOver={showbasket} onMouseLeave={hidebasket}>
+        <div id="toggleBasket" className="flex p-2 items-center gap-1 bg-white relative rounded-lg" onMouseOver={showBasketBox} onMouseLeave={hideBasketBox}>
             <Link to={`/basket`} className="flex gap-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-cart4" viewBox="0 0 16 16">
                     <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0" />
@@ -58,13 +57,13 @@ const BasketBox = () => {
                             reduce = summary.reduce((a, b) => a + b, 0).toFixed(3)
                             return (
                                 <div key={_i} className="flex items-center justify-between gap-2 w-full select-none ">
-                                    <Link to={`/category/${_v?.category}/products/${_v?.id}`} className="min-w-14 max-w-14 min-h-14 max-h-14 h-auto flex flex-col justify-center items-center border-2 ">
+                                    <Link onClick={hideBasketBox} to={`/category/${_v?.category}/products/${_v?.id}`} className="min-w-14 max-w-14 min-h-14 max-h-14 h-auto flex flex-col justify-center items-center border-2 ">
                                         <div className="h-12 w-12 flex justify-center">
                                             <img width={48} height={48} alt={_v?.title} src={_v?.image} className="w-full h-full object-contain bg-center bg-no-repeat" />
                                         </div>
                                     </Link>
                                     <div className="flex flex-col gap-1 flex-1">
-                                        <Link to={`/category/${_v?.category}/products/${_v?.id}`}>{_v?.title}</Link>
+                                        <Link onClick={hideBasketBox} to={`/category/${_v?.category}/products/${_v?.id}`}>{_v?.title}</Link>
                                         <div className="text-blue-600">{_v?.price}$</div>
                                     </div>
                                     <button data-id={_i} onClick={removeFunc} className="flex text-slate-600 hover:text-red-500">
@@ -83,7 +82,7 @@ const BasketBox = () => {
                         <div>
                             <span className="text-blue-600">{reduce || 0}$</span>
                         </div>
-                        <Link to={`/checkout`} onClick={hidebasket} className="bg-blue-600 p-2 text-white rounded-sm hover:bg-green-600">Checkout</Link>
+                        <Link to={`/checkout`} onClick={hideBasketBox} className="bg-blue-600 p-2 text-white rounded-sm hover:bg-green-600">Checkout</Link>
                     </div>
                 }
 
