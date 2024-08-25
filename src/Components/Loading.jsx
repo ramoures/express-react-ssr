1debdfd0e450fb37a6e4b38bd3c63f1808741ffc
raminme <ramoures@gmail.com>
2024-08-25 08:42:56 +0330

    update

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -2,7 +2,7 @@ const Loading = ({ n = 4 }) => {
     return (
         <>
             {n > 0 &&
-                <div className="flex flex-col justify-start items-start gap-4">
+                <div className="flex flex-col justify-start items-start gap-4 z-10">
                     {[...Array(n)].map((x, i) => {
                         return (
                             <div key={`ld_${i}`} className="w-full h-72 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-3 justify-center items-center">
@@ -16,11 +16,10 @@ const Loading = ({ n = 4 }) => {
                 </div>
             }
             {n <= 0 &&
-                <div className="flex flex-col justify-start items-start gap-4 h-screen py-6">
+                <div className="flex flex-col justify-start items-start gap-4 h-screen py-6 z-10">
 
                     <div key={`ld_0`} className="w-full h-auto grid grid-cols-1 gap-3 justify-center items-center">
                         <div key={`ld_0_1`} className="w-full min-h-[400px] bg-neutral-200 animate-pulse duration-75 ease-linear block rounded-3xl"></div>
-
                     </div>
 
                 </div>
